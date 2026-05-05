4840982af1be9448d4effd179ec9e9b69f1dfdff
Karol Lassak <ingwar@ingwar.eu.org>
2026-05-05 14:53:15 +0200

    Fix deb build (#6)
    * Display directory contents before package build
    
    Add echo and ls commands to display directory contents before building the package.
    
    * Refactor ManualBuild.sh for package directory and cleanup
    
    Updated package directory path and improved directory creation commands. Removed deprecated control file creation and deb package building steps.
    
    * Update build command in project.yml
    
    Remove moving .deb files to artifacts directory in build step.

--- ManualBuild.sh
+++ ManualBuild.sh
@@ -10,7 +10,7 @@ EXEDIR=/usr/sbin
 MAN_PAGE=sedutil-cli.8
 MAN_DIR=/usr/share/man/man8
 USR_SYSTEMD=/etc/systemd/system
-PKGDIR=debian
+PKGDIR=${WORKSPACE}/output/
 
 export LDFLAGS=-static
 
@@ -22,24 +22,12 @@ for WRKDIR in "${WORKDIRS[@]}"; do
 	make -j8 -C "$WRKDIR" CONF="${VERSION}" clean
 done
 
-rm -rf "*.deb"
 sudo rm -rf "${PKGDIR}"
 
-mkdir "${PKGDIR}"
+mkdir -p "${PKGDIR}"
 mkdir -p "${PKGDIR}/${EXEDIR}"
 mkdir -p "${PKGDIR}/${USR_SYSTEMD}"
 mkdir -p "${PKGDIR}/${MAN_DIR}"
-
-cp -rp DEBIAN "${PKGDIR}"
-cat > "$PKGDIR/DEBIAN/control" <<EOF
-Package: sedutil
-Section: utils
-Version: $PKG_VERSION
-Architecture: amd64
-Maintainer: ops-team@kentik.com
-Homepage: https://github.com/kentik/sedutil
-Description: Forked and locally-modified DTA sedutil-cli package. Statically compiled, should be good for all modern distros. Built from commit $COMMIT.
-EOF
 
 for DIR in "${WORKDIRS[@]}"; do
   ( make -j8 -C "$DIR" CONF="${VERSION}" clean ; make -j8 -C "$DIR" CONF="${VERSION}" ) 2>&1 | tee logfile.txt
@@ -56,14 +44,5 @@ chmod 0644 "${PKGDIR}/${USR_SYSTEMD}/${OPAL_UNIT}"
 
 sudo find "${PKGDIR}" -execdir chown 0:0 '{}' +;
 
-dpkg-deb --build "${PKGDIR}"
-
-pkg_ver_arch=$(dpkg-deb --info debian.deb  \
-		   | grep -E 'Package|Version|Architecture' \
-		   | tr -d ' ' \
-		   | tr ':' '=')
-for var in "${pkg_ver_arch[@]}"; do
-  eval "$var"
-done
-
-mv debian.deb "${Package}_${Version}_${Architecture}.deb"
+echo "Dir content"
+ls -R "${PKGDIR}"
